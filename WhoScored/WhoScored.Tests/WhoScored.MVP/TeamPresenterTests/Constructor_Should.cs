b8c0779b34926e699cda88896bd48980ee3e70e2
pspassova <petya.spassova@abv.bg>
2017-02-21 20:04:03 +0200

    TeamPresenter - tested

--- WhoScored/WhoScored.Tests/WhoScored.MVP/TeamPresenterTests/Constructor_Should.cs
+++ WhoScored/WhoScored.Tests/WhoScored.MVP/TeamPresenterTests/Constructor_Should.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using WhoScored.MVP.Models;
+using WhoScored.MVP.Presenters;
+using WhoScored.MVP.Views;
+using WhoScored.Services.Contracts;
 
 namespace WhoScored.Tests.WhoScored.MVP.TeamPresenterTests
 {
-    class Constructor_Should
+    [TestFixture]
+    public class Constructor_Should
     {
+        [Test]
+        public void ThrowArgumentNullException_WhenPassedServiceIsNull()
+        {
+            var viewMock = new Mock<ITeamView>();
+            viewMock.Setup(v => v.Model).Returns(new TeamViewModel());
+
+            var actualMessage = Assert.Throws<ArgumentNullException>(() => new TeamPresenter(viewMock.Object, null));
+
+            StringAssert.IsMatch("teamService", actualMessage.ParamName);
+        }
+
+        [Test]
+        public void CreateCorrectInstanceOfTeamPresenter_WhenPassedArgumentsAreValid()
+        {
+            var teamServiceMock = new Mock<ITeamService>();
+            var viewMock = new Mock<ITeamView>();
+            viewMock.Setup(v => v.Model).Returns(new TeamViewModel());
+
+            TeamPresenter teamPresenter = new TeamPresenter(viewMock.Object,teamServiceMock.Object);
+
+            Assert.IsInstanceOf<TeamPresenter>(teamPresenter);
+        }
     }
 }
--- WhoScored/WhoScored.Tests/WhoScored.MVP/TeamPresenterTests/View_OnOnGetTeam_Should.cs
+++ WhoScored/WhoScored.Tests/WhoScored.MVP/TeamPresenterTests/View_OnOnGetTeam_Should.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
+using NUnit.Framework;
+using WhoScored.Models.Models;
+using WhoScored.MVP.Models;
+using WhoScored.MVP.Models.CustomEventArgs;
+using WhoScored.MVP.Presenters;
+using WhoScored.MVP.Views;
+using WhoScored.Services.Contracts;
 
 namespace WhoScored.Tests.WhoScored.MVP.TeamPresenterTests
 {
-    class View_OnOnGetTeam_Should
+    [TestFixture]
+    public class View_OnOnGetTeam_Should
     {
+        [Test]
+        public void AddTeamToViewModel_WhenOnGetTeamEventIsRaised()
+        {
+            var viewMock = new Mock<ITeamView>();
+            viewMock.Setup(v => v.Model).Returns(new TeamViewModel());
+
+            Team team = new Team();
+            var teamServiceMock = new Mock<ITeamService>();
+            teamServiceMock.Setup(x => x.GetTeamById(It.IsAny<int>())).Returns(team);
+
+            TeamPresenter teamPresenter = new TeamPresenter(viewMock.Object, teamServiceMock.Object);
+
+            viewMock.Raise(x => x.OnGetTeam += null, new IdEventArgs(It.IsAny<int>()));
+
+            Assert.AreEqual(team, viewMock.Object.Model.Team);
+        }
     }
 }
