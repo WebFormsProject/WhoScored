0e4a864d84e3e68de71c2c99acb483b8931ee1be
TeodSt <teod_stoyanova@abv.bg>
2017-02-21 20:14:18 +0200

    FootballPlayerPresenter tests

--- WhoScored/WhoScored.Tests/WhoScored.MVP/FootballPlayerPresenterTests/View_OnGetFootballPlayerById_Should.cs
+++ WhoScored/WhoScored.Tests/WhoScored.MVP/FootballPlayerPresenterTests/View_OnGetFootballPlayerById_Should.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
+using NUnit.Framework;
+using WhoScored.Models.Models;
+using WhoScored.MVP.Models;
+using WhoScored.MVP.Models.CustomEventArgs;
+using WhoScored.MVP.Presenters;
+using WhoScored.MVP.Views;
+using WhoScored.Services.Contracts;
 
 namespace WhoScored.Tests.WhoScored.MVP.FootballPlayerPresenterTests
 {
-    class View_OnGetFootballPlayerById_Should
+    [TestFixture]
+    public class View_OnGetFootballPlayerById_Should
     {
+        [Test]
+        public void SetFootballPlayerToViewModel_WhenOnGetFootballPlayerByIdIsRaised()
+        {
+            var viewMock = new Mock<IFootballPlayerView>();
+            var footballServiceMock = new Mock<IFootballPlayerService>();
+
+            FootballPlayer footballPlayer = new FootballPlayer();
+            footballServiceMock.Setup(x => x.GetFootballPlayerById(It.IsAny<int>())).Returns(footballPlayer);
+
+            FootballPlayerViewModel model = new FootballPlayerViewModel();
+            viewMock.Setup(x => x.Model).Returns(model);
+
+            FootballPlayerPresenter presenter = new FootballPlayerPresenter(viewMock.Object, footballServiceMock.Object);
+
+            viewMock.Raise(x => x.OnGetFootballPlayerById += null, new IdEventArgs(8));
+
+            Assert.AreEqual(footballPlayer, viewMock.Object.Model.FootballPlayer);
+        }
     }
 }
